2b67a5a20c5e97a734a99c512a6c261afc584393
remoto ufsc <iagosilvestrehp@hotmail.com>
2022-11-15 11:33:04 -0300

    monitor changes

--- ROSMonitoring/monitor/src/monitor_0.py
+++ ROSMonitoring/monitor/src/monitor_0.py
@@ -10,20 +10,83 @@ from monitor.msg import *
 from std_msgs.msg import String
 
 ws_lock = Lock()
+dict_msgs = {}
 from std_msgs.msg import Int8
 
+pubdetect_fire_uav1 = rospy.Publisher(name = 'detect_fire_uav1_mon', data_class = Int8, latch = True, queue_size = 1000)
 def callbackdetect_fire_uav1(data):
 	global ws, ws_lock
-	rospy.loginfo('monitor has observed: ' + str(data))
 	dict = message_converter.convert_ros_message_to_dictionary(data)
 	dict['topic'] = 'detect_fire_uav1'
 	dict['time'] = rospy.get_time()
 	ws_lock.acquire()
-	logging(dict)
+	while dict['time'] in dict_msgs:
+		dict['time'] += 0.01
+	ws.send(json.dumps(dict))
+	dict_msgs[dict['time']] = data
 	ws_lock.release()
-	rospy.loginfo('event has been successfully logged')
-pub_dict = {}
-msg_dict = { 'detect_fire_uav1' : "std_msgs/Int8"}
+pubdetect_fire_uav2 = rospy.Publisher(name = 'detect_fire_uav2_mon', data_class = Int8, latch = True, queue_size = 1000)
+def callbackdetect_fire_uav2(data):
+	global ws, ws_lock
+	dict = message_converter.convert_ros_message_to_dictionary(data)
+	dict['topic'] = 'detect_fire_uav2'
+	dict['time'] = rospy.get_time()
+	ws_lock.acquire()
+	while dict['time'] in dict_msgs:
+		dict['time'] += 0.01
+	ws.send(json.dumps(dict))
+	dict_msgs[dict['time']] = data
+	ws_lock.release()
+pubdetect_fire_uav3 = rospy.Publisher(name = 'detect_fire_uav3_mon', data_class = Int8, latch = True, queue_size = 1000)
+def callbackdetect_fire_uav3(data):
+	global ws, ws_lock
+	dict = message_converter.convert_ros_message_to_dictionary(data)
+	dict['topic'] = 'detect_fire_uav3'
+	dict['time'] = rospy.get_time()
+	ws_lock.acquire()
+	while dict['time'] in dict_msgs:
+		dict['time'] += 0.01
+	ws.send(json.dumps(dict))
+	dict_msgs[dict['time']] = data
+	ws_lock.release()
+pubdetect_fire_uav4 = rospy.Publisher(name = 'detect_fire_uav4_mon', data_class = Int8, latch = True, queue_size = 1000)
+def callbackdetect_fire_uav4(data):
+	global ws, ws_lock
+	dict = message_converter.convert_ros_message_to_dictionary(data)
+	dict['topic'] = 'detect_fire_uav4'
+	dict['time'] = rospy.get_time()
+	ws_lock.acquire()
+	while dict['time'] in dict_msgs:
+		dict['time'] += 0.01
+	ws.send(json.dumps(dict))
+	dict_msgs[dict['time']] = data
+	ws_lock.release()
+pubdetect_fire_uav5 = rospy.Publisher(name = 'detect_fire_uav5_mon', data_class = Int8, latch = True, queue_size = 1000)
+def callbackdetect_fire_uav5(data):
+	global ws, ws_lock
+	dict = message_converter.convert_ros_message_to_dictionary(data)
+	dict['topic'] = 'detect_fire_uav5'
+	dict['time'] = rospy.get_time()
+	ws_lock.acquire()
+	while dict['time'] in dict_msgs:
+		dict['time'] += 0.01
+	ws.send(json.dumps(dict))
+	dict_msgs[dict['time']] = data
+	ws_lock.release()
+pubdetect_fire_uav6 = rospy.Publisher(name = 'detect_fire_uav6_mon', data_class = Int8, latch = True, queue_size = 1000)
+def callbackdetect_fire_uav6(data):
+	global ws, ws_lock
+	dict = message_converter.convert_ros_message_to_dictionary(data)
+	dict['topic'] = 'detect_fire_uav6'
+	dict['time'] = rospy.get_time()
+	ws_lock.acquire()
+	while dict['time'] in dict_msgs:
+		dict['time'] += 0.01
+	ws.send(json.dumps(dict))
+	dict_msgs[dict['time']] = data
+	ws_lock.release()
+pub_dict = { 'detect_fire_uav1' : pubdetect_fire_uav1,  'detect_fire_uav2' : pubdetect_fire_uav2,  'detect_fire_uav3' : pubdetect_fire_uav3,  'detect_fire_uav4' : pubdetect_fire_uav4,  'detect_fire_uav5' : pubdetect_fire_uav5,  'detect_fire_uav6' : pubdetect_fire_uav6}
+msg_dict = { 'detect_fire_uav1' : "std_msgs/Int8",  'detect_fire_uav2' : "std_msgs/Int8",  'detect_fire_uav3' : "std_msgs/Int8",  'detect_fire_uav4' : "std_msgs/Int8",  'detect_fire_uav5' : "std_msgs/Int8",  'detect_fire_uav6' : "std_msgs/Int8"}
 def monitor():
 	global pub_error, pub_verdict
 	with open(log, 'w') as log_file:
@@ -32,13 +95,61 @@ def monitor():
 	pub_error = rospy.Publisher(name = 'monitor_0/monitor_error', data_class = MonitorError, latch = True, queue_size = 1000)
 	pub_verdict = rospy.Publisher(name = 'monitor_0/monitor_verdict', data_class = String, latch = True, queue_size = 1000)
 	rospy.Subscriber('detect_fire_uav1', Int8, callbackdetect_fire_uav1)
-	rospy.loginfo('monitor started and ready')
+	rospy.Subscriber('detect_fire_uav2', Int8, callbackdetect_fire_uav2)
+	rospy.Subscriber('detect_fire_uav3', Int8, callbackdetect_fire_uav3)
+	rospy.Subscriber('detect_fire_uav4', Int8, callbackdetect_fire_uav4)
+	rospy.Subscriber('detect_fire_uav5', Int8, callbackdetect_fire_uav5)
+	rospy.Subscriber('detect_fire_uav6', Int8, callbackdetect_fire_uav6)
+def on_message(ws, message):
+	global error, log, actions
+	json_dict = json.loads(message)
+	if json_dict['verdict'] == 'true' or json_dict['verdict'] == 'currently_true' or json_dict['verdict'] == 'unknown':
+		if json_dict['verdict'] == 'true' and not pub_dict:
+			rospy.loginfo('The monitor concluded the satisfaction of the property under analysis, and can be safely removed.')
+			ws.close()
+			exit(0)
+		else:
+			logging(json_dict)
+			topic = json_dict['topic']
+			if topic in pub_dict:
+				pub_dict[topic].publish(dict_msgs[json_dict['time']])
+			del dict_msgs[json_dict['time']]
+	else:
+		logging(json_dict)
+		if (json_dict['verdict'] == 'false' and actions[json_dict['topic']][1] >= 1) or (json_dict['verdict'] == 'currently_false' and actions[json_dict['topic']][1] == 1):
+			error = MonitorError()
+			error.topic = json_dict['topic']
+			error.time = json_dict['time']
+			error.property = json_dict['spec']
+			error.content = str(dict_msgs[json_dict['time']])
+			pub_error.publish(error)
+			if json_dict['verdict'] == 'false' and not pub_dict:
+				rospy.loginfo('The monitor concluded the violation of the property under analysis, and can be safely removed.')
+				ws.close()
+				exit(0)
+		if actions[json_dict['topic']][0] != 'filter':
+			if json_dict['verdict'] == 'currently_false':
+				rospy.loginfo('The event ' + message + ' is consistent ')
+			topic = json_dict['topic']
+			if topic in pub_dict:
+				pub_dict[topic].publish(dict_msgs[json_dict['time']])
+			del dict_msgs[json_dict['time']]
+		error = True
+	pub_verdict.publish(json_dict['verdict'])
+
+def on_error(ws, error):
+	rospy.loginfo(error)
+
+def on_close(ws):
+	rospy.loginfo('### websocket closed ###')
+
+def on_open(ws):
+	rospy.loginfo('### websocket is open ###')
 
 def logging(json_dict):
 	try:
 		with open(log, 'a+') as log_file:
 			log_file.write(json.dumps(json_dict) + '\n')
-		rospy.loginfo('event logged')
 	except:
 		rospy.loginfo('Unable to log the event.')
 
@@ -46,10 +157,23 @@ def main(argv):
 	global log, actions, ws
 	log = '/home/ctc_das/mrs_workspace/src/log.txt' 
 	actions = {
-		'detect_fire_uav1' : ('log', 0)
+		'detect_fire_uav1' : ('filter', 0), 
+		'detect_fire_uav2' : ('filter', 0), 
+		'detect_fire_uav3' : ('filter', 0), 
+		'detect_fire_uav4' : ('filter', 0), 
+		'detect_fire_uav5' : ('filter', 0), 
+		'detect_fire_uav6' : ('filter', 0)
 	}
 	monitor()
-	rospy.spin()
+	websocket.enableTrace(False)
+	ws = websocket.WebSocketApp(
+		'ws://127.0.0.1:8080',
+		on_message = on_message,
+            
+		on_error = on_error,
+		on_close = on_close,
+		on_open = on_open)
+	ws.run_forever()
 
 if __name__ == '__main__':
 	main(sys.argv)
--- ROSMonitoring/oracle/RMLOracle/rml/test.pl
+++ ROSMonitoring/oracle/RMLOracle/rml/test.pl
@@ -1,10 +1,10 @@
 :- module('spec', [trace_expression/2, match/2]).
 :- use_module(monitor('deep_subdict')).
-match(_event, d1) :- deep_subdict(_{'data':Val,'topic':"detect_fire_uav1"}, _event), =(Val, 0).
-match(_event, d2) :- deep_subdict(_{'data':Val,'topic':"detect_fire_uav2"}, _event), =(Val, 0).
-match(_event, d3) :- deep_subdict(_{'data':Val,'topic':"detect_fire_uav3"}, _event), =(Val, 0).
-match(_event, d4) :- deep_subdict(_{'data':Val,'topic':"detect_fire_uav4"}, _event), =(Val, 0).
-match(_event, d5) :- deep_subdict(_{'data':Val,'topic':"detect_fire_uav5"}, _event), =(Val, 0).
-match(_event, d6) :- deep_subdict(_{'data':Val,'topic':"detect_fire_uav6"}, _event), =(Val, 0).
+match(_event, d1) :- deep_subdict(_{'data':Val,'topic':"detect_fire_uav1"}, _event), =(Val, 1).
+match(_event, d2) :- deep_subdict(_{'data':Val,'topic':"detect_fire_uav2"}, _event), =(Val, 2).
+match(_event, d3) :- deep_subdict(_{'data':Val,'topic':"detect_fire_uav3"}, _event), =(Val, 3).
+match(_event, d4) :- deep_subdict(_{'data':Val,'topic':"detect_fire_uav4"}, _event), =(Val, 4).
+match(_event, d5) :- deep_subdict(_{'data':Val,'topic':"detect_fire_uav5"}, _event), =(Val, 5).
+match(_event, d6) :- deep_subdict(_{'data':Val,'topic':"detect_fire_uav6"}, _event), =(Val, 6).
 match(_, any).
-trace_expression('Main', Main) :- Main=star(d1:eps).
+trace_expression('Main', Main) :- Main=plus((d1 \/ d2 \/ d3 \/ d4 \/ d5 \/ d6)).	
